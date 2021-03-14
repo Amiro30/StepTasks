1a7d6f214ea136f7828d57a4406ffd905c3a4c5d
Andrew Miroshnichenko <amiro30@i.ua>
2021-03-14 22:16:12 +0200

    Add project files.

---
+++ TaskSerialize/Bill.cs
@@ -0,0 +1,88 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+namespace TaskSerialize
+{
+    class Bill
+    {
+        [JsonProperty(PropertyName = "PaymentPerday")]
+        private double _paymentPerday;
+
+        [JsonProperty(PropertyName = "Days")]
+        private int _days;
+
+        [JsonProperty(PropertyName = "PenaltyPerDayDelay")]
+        private double _penaltyPerDayDelay;
+
+        [JsonIgnore]
+        public double Sum { get; private set; }
+        [JsonIgnore]
+        public double PenaltySum { get; private set; }
+        [JsonIgnore]
+        public double PaymentSum { get; private set; }
+
+        private static JsonSerializer serializer;
+        public static bool SerializeComputableFields { get; set; }
+
+        public Bill(double paymentPerday, int days, double penaltyPerDayDelay)
+        {
+            _paymentPerday = paymentPerday;
+            _days = days;
+            _penaltyPerDayDelay = penaltyPerDayDelay;
+            CalculateAllPayments();
+        }
+
+        private void CalculateAllPayments()
+        {
+            Sum = _paymentPerday * _days;
+            PenaltySum = _penaltyPerDayDelay * _days;
+            PaymentSum = Sum + PenaltySum;
+        }
+
+        public void Serialize(StreamWriter writer, Bill obj, bool serializeComputableFields)
+        { 
+          if(!serializeComputableFields)
+            {
+                serializer = new JsonSerializer() {Formatting = Formatting.Indented};
+                serializer.Serialize(writer, obj);
+            }
+          else
+            {
+                JsonSerializerSettings settings = new JsonSerializerSettings();
+                settings.ContractResolver = new IgnoreJsonAttributesResolver();
+                settings.Formatting = Formatting.Indented;
+
+                serializer = JsonSerializer.Create(settings);
+                serializer.Serialize(writer, obj);
+            }
+        }
+
+        public static Bill Deserialize (StreamReader reader)
+        {
+            return serializer.Deserialize(reader, typeof(Bill)) as Bill;
+         
+        }
+    }
+
+    class IgnoreJsonAttributesResolver : DefaultContractResolver
+    {
+        protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization)
+        {
+            IList<JsonProperty> props = base.CreateProperties(type, memberSerialization);
+            foreach (var prop in props)
+            {
+                prop.Ignored = false;  
+            }
+            return props;
+        }
+    }
+}
+
+
+
+
---
+++ TaskSerialize/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace TaskSerialize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool serializeComputableFields = false;
+            var bill = new Bill(paymentPerday: 200, days: 7, penaltyPerDayDelay: 20);
+
+            //Saving to file
+            Save(bill, "TaskSerialize.json", serializeComputableFields);
+
+            //Reading from file
+            var loadBill = ReadBill("TaskSerialize.json");
+
+            Console.WriteLine($" cумма к оплате без штрафа: {loadBill.Sum} \n штраф: {loadBill.PenaltySum} \n общая сумма к оплате: {loadBill.PaymentSum}");
+            Console.ReadKey();
+        }
+
+        public static void Save(Bill bill, string fileName, bool serializeComputableFields)
+        {
+            using (StreamWriter writer = File.CreateText(fileName))
+            {
+                bill.Serialize(writer, bill, serializeComputableFields);
+            }
+        }
+        public static Bill ReadBill(string fileName)
+        {
+            using (Stream file = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+            using (StreamReader reader = new StreamReader(file, Encoding.UTF8))
+            {
+                return Bill.Deserialize(reader);
+            }
+        }
+    }
+}
+
+
+/*Разработать класс «Счет для оплаты». В классе предусмотреть следующие поля:
+-оплата за день;
+-количество дней;
+-штраф за один день задержки оплаты;
+-количество дней задержи оплаты;
+-сумма к оплате без штрафа (вычисляемое поле);
+-штраф(вычисляемое поле);
+-общая сумма к оплате (вычисляемое поле).
+В классе объявить статическое свойство типа bool,
+значение которого влияет на процесс форматирования
+объектов этого класса. Если значение этого свойства равно true, тогда сериализуются и десериализируются все
+поля, если false — вычисляемые поля не сериализуются.
+Разработать приложение, в котором необходимо продемонстрировать использование этого класса, результаты
+должны записываться и считываться из файла.*/
