7d187ffbf09c4838df75bbd9888c4a5feb98b1cc
Andrew Miroshnichenko <amiro30@i.ua>
2021-04-11 13:11:42 +0300

    logic done

--- WPF_Calculator/MainWindow.xaml.cs
+++ WPF_Calculator/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MaterialDesignThemes.Wpf;
+using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WPF_Calculator
 {
@@ -23,6 +13,108 @@ namespace WPF_Calculator
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            
+            Button b = (Button)sender;
+            
+            var icon = (PackIconKind)Enum
+                .Parse (typeof(PackIconKind), $"{(b.Content as PackIcon).Kind}");
+            var buttonContent = PackIconConverter.GetIconValue(icon);
+
+            txtInput.Text += buttonContent;
+         }
+
+        private void Result_click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                result();
+                
+            }
+            catch (Exception exc)
+            {
+                txtInput.Text = "Error!";
+            }
+        }
+
+        private void result()
+        {
+            String op;
+            int iOp = 0;
+            if (txtInput.Text.Contains("+"))
+            {
+                iOp = txtInput.Text.IndexOf("+");
+            }
+            else if (txtInput.Text.Contains("-"))
+            {
+                iOp = txtInput.Text.IndexOf("-");
+            }
+            else if (txtInput.Text.Contains("*"))
+            {
+                iOp = txtInput.Text.IndexOf("*");
+            }
+            else if (txtInput.Text.Contains("/"))
+            {
+                iOp = txtInput.Text.IndexOf("/");
+            }
+            else
+            {
+                //error    
+            }
+
+            op = txtInput.Text.Substring(iOp, 1);
+            double op1 = Convert.ToDouble(txtInput.Text.Substring(0, iOp));
+            double op2 = Convert.ToDouble(txtInput.Text.Substring(iOp + 1, txtInput.Text.Length - iOp - 1));
+
+            double result = 0;
+            if (op == "+")
+            {
+                result = op1 + op2;
+            }
+            else if (op == "-")
+            {
+                result = op1 - op2;
+            }
+            else if (op == "*")
+            {
+                result = op1 * op2;
+            }
+            else
+            {
+                result = op1 / op2;
+            }
+            previousOp.Text = txtInput.Text;
+            resultOp.Text += "" + result;
+            txtInput.Text = "";
+        }
+
+        private void Off_Click_1(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+            
+        }
+
+        private void Del_Click(object sender, RoutedEventArgs e)
+        {
+            txtInput.Text = "";
+        }
+
+        private void R_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtInput.Text.Length > 0)
+            {
+                txtInput.Text = txtInput.Text.Substring(0, txtInput.Text.Length - 1);
+            }
+        }
+
+        private void Del_All_Click(object sender, RoutedEventArgs e)
+        {
+            txtInput.Text = "";
+            resultOp.Text = "=";
+            previousOp.Text = "";
         }
     }
 }
---
+++ WPF_Calculator/PackIconConverter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MaterialDesignThemes.Wpf;
+
+namespace WPF_Calculator
+{
+    public static class PackIconConverter
+    {
+        private static Dictionary<PackIconKind, string> iconDictionary = new Dictionary<PackIconKind, string>
+        {
+            [PackIconKind.Number1] = "1",
+            [PackIconKind.Number2] = "2",
+            [PackIconKind.Number3] = "3",
+            [PackIconKind.Number4] = "4",
+            [PackIconKind.Number5] = "5",
+            [PackIconKind.Number6] = "6",
+            [PackIconKind.Number7] = "7",
+            [PackIconKind.Number8] = "8",
+            [PackIconKind.Number9] = "9",
+            [PackIconKind.Number0] = "0",
+            [PackIconKind.Division] = "/",
+            [PackIconKind.Multiplication] = "*",
+            [PackIconKind.Minus] = "-",
+            [PackIconKind.Plus] = "+",
+        };
+        public static string GetIconValue(PackIconKind icon)
+        {
+             var result = iconDictionary.FirstOrDefault(x => x.Key == icon).Value;
+
+             return result;
+        }
+    }
+}
