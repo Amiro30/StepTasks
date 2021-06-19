ec4c8e9a26e674adf0d247a8c6d5496ee2dadf5d
Andrew Miroshnichenko <amiro30@i.ua>
2021-06-19 23:14:20 +0300

    file oprations added

---
+++ WPF_SystemProgrmming/FileOperator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_SystemProgrmming
+{
+    class FileOperator
+    {
+        public IList<string> GetMatchingFiles(string[] searchwords, IList<FileInfo> files)
+        {
+            var matchedFiles = new List<string>();
+            foreach (var word in searchwords)
+            {
+                var queryMatchingFiles =
+                    from file in files
+                    let fileText = GetFileText(file.FullName)
+                    where fileText.Contains(word)
+                    select file.FullName;
+
+                matchedFiles.AddRange(queryMatchingFiles);
+            }
+
+            return matchedFiles;
+        }
+
+        public IList<FileInfo> GetFilesInfo(string fileExtension)
+        {
+            //search in all drives  and all directories to search for all .txt files
+            List<FileInfo> filesInfos = new List<FileInfo>();
+
+            foreach (DriveInfo drive in DriveInfo.GetDrives().Where(x => x.IsReady))
+            {
+                var dirs = drive.RootDirectory.GetDirectories();
+                foreach (var dir in dirs)
+                {
+                    try
+                    {
+                        var fileList = dir.GetFiles("*." + $"{fileExtension}", System.IO.SearchOption.AllDirectories);
+                        filesInfos.AddRange(fileList);
+                    }
+                    catch (UnauthorizedAccessException e)
+                    {
+                        Debug.WriteLine($"Folder inaccessible due to permissions {e.Message}");
+                    }
+                }
+            }
+            return filesInfos;
+        }
+
+        private string GetFileText(string name)
+        {
+            string fileContents = String.Empty;
+
+            // If the file has been deleted since we took
+            // the snapshot, ignore it and return the empty string.  
+            if (System.IO.File.Exists(name))
+            {
+                fileContents = System.IO.File.ReadAllText(name);
+            }
+            return fileContents;
+        }
+    }
+}
+
--- WPF_SystemProgrmming/MainWindow.xaml.cs
+++ WPF_SystemProgrmming/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -25,80 +26,34 @@ namespace WPF_SystemProgrmming
         public MainWindow()
         {
             InitializeComponent();
+
+            txtInput.Select(0,0);
+
         }
 
         private void Start_Click(object sender, RoutedEventArgs e)
         {
-            var words = txtInput.Text.Split(' ');
 
-            var fileList = GetFiles();
+            FileOperator fileOperator = new FileOperator();
 
-            string searchTerm = @"Visual";
+            //var words =  HandleInput(txtInput.Text);
 
-            // Search the contents of each file.  
-            // A regular expression created with the RegEx class  
-            // could be used instead of the Contains method.  
-            // queryMatchingFiles is an IEnumerable<string>.  
-            var queryMatchingFiles =
-                from file in fileList
-                //where file.Extension == ".htm"
-                let fileText = GetFileText(file)
-                where fileText.Contains(searchTerm)
-                select file;
+            string[] words = new[] {"visualfafa", "fghj"};
 
-            // Execute the query.  
-            Debug.WriteLine("\nThe term \"{0}\" was found in:", searchTerm);
-            foreach (string filename in queryMatchingFiles)
-            {
-                Debug.WriteLine(filename);
-            }
+            var filesInfo = fileOperator.GetFilesInfo("txt");
 
+            var matchedFiles = fileOperator.GetMatchingFiles(words, filesInfo);
 
 
         }
 
-        private List<string> GetFiles()
+        private string[] HandleInput(string input)
         {
-            //in all drives to search for all .txt files
-            var files = new List<string>();
+            input = Regex.Replace(input, @"\s+", "");
 
+            var words = input.Split(',');
 
-            foreach (DriveInfo drive in DriveInfo.GetDrives().Where(x => x.IsReady))
-            {
-
-                try
-                {
-                    var dirs = drive.RootDirectory.GetDirectories();
-                    foreach (var dir in dirs)
-                    {
-                        files.AddRange(dir.GetFiles("*.txt").Select(x=>x.FullName));
-                    }
-                    
-                }
-                catch (UnauthorizedAccessException e)
-                {
-                    Debug.WriteLine($"something gone wrong {e.Message}");
-                }
-
-            }
-
-            return files;
-        }
-
-
-        // Read the contents of the file.  
-        private string GetFileText(string name)
-        {
-            string fileContents = String.Empty;
-
-            // If the file has been deleted since we took
-            // the snapshot, ignore it and return the empty string.  
-            if (System.IO.File.Exists(name))
-            {
-                fileContents = System.IO.File.ReadAllText(name);
-            }
-            return fileContents;
+            return words;
         }
     }
-    
 }
